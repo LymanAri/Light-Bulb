a35bb31daee79012ad67cb9b195916f8ffe29ddb
LymanAri <lyman.ari@gmail.com>
2020-03-22 18:17:00 +0200

    2 functions to turn on and off the Bulb Element added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,29 @@
 import '../styles/index.scss';
+import '@fortawesome/fontawesome-free/js/all';
 
-console.log('webpack starterkit');
+function bulbOn() {
+    let color = document.getElementsByName("color")[0].value;
+    if (!color) {
+        color = "ffeb00";
+    }
+    const bulbElement = document.getElementsByClassName("fa-lightbulb")[0];
+    bulbElement.style.color = color;
+    const bulbStatus = document.getElementById("bulbStatus");
+    bulbStatus.innerHTML = "ON";
+    bulbStatus.style.color = "#000000";
+    const bodyElement = document.getElementsByTagName("body")[0];
+    bodyElement.style.backgroundColor = "#ffffff";
+}
+
+function bulbOff() {
+    const bulbElement = document.getElementsByClassName("fa-lightbulb")[0];
+    bulbElement.style.color = "#000000";
+    const bulbStatus = document.getElementById("bulbStatus");
+    bulbStatus.innerHTML = "OFF";
+    bulbStatus.style.color = "#ffffff";
+    const bodyElement = document.getElementsByTagName("body")[0];
+    bodyElement.style.backgroundColor = "#7b7a7a";
+}
+
+window.bulbOn = bulbOn;
+window.bulbOff = bulbOff;
